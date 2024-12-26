19ad34ec853c478493687231aa1396ccdbea9375
Faisal <faisaladamj@gmail.com>
2024-12-26 18:42:56 -0500

    removed middleware

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -1,30 +1,30 @@
-import { geolocation } from '@vercel/functions'
-import { type NextRequest, NextResponse } from 'next/server'
+// import { geolocation } from '@vercel/functions'
+// import { type NextRequest, NextResponse } from 'next/server'
 
-// run only on /test route
-export const config = {
-  matcher: '/test',
-}
+// // run only on /test route
+// export const config = {
+//   matcher: '/test',
+// }
 
-export async function middleware(req: NextRequest) {
-  console.log('Middleware Executed'); // Log this line
+// export async function middleware(req: NextRequest) {
+//   console.log('Middleware Executed'); // Log this line
 
-  const url = req.nextUrl.clone()
-  const geo = geolocation(req)
+//   const url = req.nextUrl.clone()
+//   const geo = geolocation(req)
 
-  const country = geo.country || 'US'
-  const city = geo.city || 'San Francisco'
-  const region = geo.region || 'CA'
+//   const country = geo.country || 'US'
+//   const city = geo.city || 'San Francisco'
+//   const region = geo.region || 'CA'
 
-  const latitude = geo.latitude ? geo.latitude.toString() : null
-  const longitude = geo.longitude ? geo.longitude.toString() : null
+//   const latitude = geo.latitude ? geo.latitude.toString() : null
+//   const longitude = geo.longitude ? geo.longitude.toString() : null
 
-  url.searchParams.set('country', country)
-  url.searchParams.set('city', city)
-  url.searchParams.set('region', region)
-  url.searchParams.set('latitude', latitude || 'null')
-  url.searchParams.set('longitude', longitude || 'null')
+//   url.searchParams.set('country', country)
+//   url.searchParams.set('city', city)
+//   url.searchParams.set('region', region)
+//   url.searchParams.set('latitude', latitude || 'null')
+//   url.searchParams.set('longitude', longitude || 'null')
 
-  console.log('Rewriting URL with params:', url.toString()); // Log the rewritten URL
-  return NextResponse.rewrite(url)
-}
+//   console.log('Rewriting URL with params:', url.toString()); // Log the rewritten URL
+//   return NextResponse.rewrite(url)
+// }
